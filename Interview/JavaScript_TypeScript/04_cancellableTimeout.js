b1d30f26b859896c32e6182bbe8580becb7b3c96
simzeee <jwsims816@gmail.com>
2025-03-07 15:28:36 -0500

    chore: add .gitignore, finish 3 exercises

---
+++ Interview/JavaScript_TypeScript/04_cancellableTimeout.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Function} callback
+ * @param {number} delay
+ * @param {...any} args
+ * @returns {Function}
+ */
+export default function setCancellableTimeout(callback, delay, ...args) {
+  const timerId = setTimeout(callback, delay, ...args)
+
+  return () => {
+    clearTimeout(timerId)
+  }
+
+}
